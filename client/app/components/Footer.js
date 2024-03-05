87ddaf870429170ff4aa006e362d0006e2dfb306
anthonyss09 <76715818+anthonyss09@users.noreply.github.com>
2024-03-05 14:42:36 -0500

    links added to footer

--- client/app/components/Footer.js
+++ client/app/components/Footer.js
@@ -2,15 +2,25 @@ import Image from "next/image";
 import LinkdIn from "../assets/svgs/linkedInR.svg";
 import GmailIcon from "../assets/svgs/gmailIconR.svg";
 import GitHubPages from "../assets/svgs/gitHubPagesR.svg";
+import Link from "next/link";
 
 export default function Footer({ footerClass, footerId }) {
   return (
     <div id={footerId} className={`footer ${footerClass}`}>
-      <Image src={GmailIcon} alt="gmail icon" />
-
-      <Image src={GitHubPages} alt="github icon" />
-
-      <Image src={LinkdIn} alt="linked in icon" />
+      <Link href="#" className="link">
+        <Image src={GmailIcon} alt="gmail icon" />
+      </Link>
+      <Link href="https://github.com/anthonyss09" className="link">
+        {" "}
+        <Image src={GitHubPages} alt="github icon" />
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/anthony-toffetti-58345b122/"
+        className="link"
+      >
+        {" "}
+        <Image src={LinkdIn} alt="linked in icon" />
+      </Link>
     </div>
   );
 }
