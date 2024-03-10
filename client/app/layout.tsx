7aa92ae95bef48d383ffbe8b6aff015de0a5856a
anthonyss09 <76715818+anthonyss09@users.noreply.github.com>
2024-03-10 15:56:03 -0400

    nginx updated with nodejs upstream directive

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -6,7 +6,8 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Cloud 9",
-  description: "A product of ATOFFETI DEV",
+  description:
+    "Cloud 9 is a software development company produced by ATOFFETTIDEV LLC. Cloud 9 displays previous work and a blog with articles and tutorials meant to help developers implement new technology into their websites. Cloud 9's blog will often talk about java script, nodejs, react, nextjs, redux, express, animations, deployments and more.",
 };
 
 export default function RootLayout({
